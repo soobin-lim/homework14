3b43668a5a470f8d33b67e27e21e4edb865fa059
Soobin Lim <bootcamp@Soobins-Mac-mini.local>
2021-11-04 15:15:01 -0600

    nev bar alignment, button alignment in Dashboard menu

--- app.js
+++ app.js
@@ -10,7 +10,27 @@ var loginRouter = require('./routes/login');
 var logoutRouter = require('./routes/logout');
 var signupRouter = require('./routes/signup');
 
-var app = express();
+//Sequelize--------------------------------------------------------------------------------
+require('dotenv').config()
+const { Sequelize } = require('sequelize');
+const mysql = require('mysql2');
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASS, {
+  host: 'localhost',
+  dialect: 'mysql' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+});
+const sequelizeConnectionTest = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Sequelize - Connection has been established successfully.');
+  } catch (error) {
+    console.error('Sequelize - Unable to connect to the database:', error);
+  }
+}
+sequelizeConnectionTest();
+//Sequelize--------------------------------------------------------------------------------
+
+var app = express({ path: '.env' });
 
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
@@ -19,13 +39,19 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('views','./views');
+app.set('views', './views');
+
+console.log(__dirname);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css2', express.static(path.join(__dirname, 'public/stylesheets/')))
+
+
+console.log(__dirname);
 
 app.use('/', homeRouter);
 app.use('/home', homeRouter);
@@ -36,12 +62,12 @@ app.use('/signup', signupRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/logout.js
+++ routes/logout.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('logout', { title: 'Express', nevbar_text: 'logout' });
+  res.render('logout', { title: 'Express', nevbar_text: 'Logout' });
 });
 
 module.exports = router;
